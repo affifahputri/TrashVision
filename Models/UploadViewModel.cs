3adf512a22150061b5cccd044d0ca69017c4780e
alifyazz <alifyazz05@gmail.com>
2026-02-02 02:10:10 +0700

    clean up: remove ignored files

--- Models/UploadViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjekTrashVision.Models
-{
-    public class UploadViewModel
-    {
-        public string? HasilPrediksi { get; set; }
-        public float SkorKepastian { get; set; }
-        public string? GambarBase64 { get; set; } // Untuk menampilkan kembali gambar yang diupload
-    }
-}
